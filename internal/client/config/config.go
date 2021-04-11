9780935ac92db7fcb8b8f351f6143246312cfa42
708u <naoya.uda@showcase-gig.com>
2021-04-11 21:43:49 +0900

    feat: add config

--- internal/client/config/config.go
+++ internal/client/config/config.go
@@ -12,9 +12,11 @@ const (
 )
 
 type Config struct {
-	Env  string
-	HTTP HTTP
-	Auth Auth
+	Env string
+
+	HTTP   HTTP
+	Auth   Auth
+	Client Client
 }
 
 type HTTP struct {
@@ -22,13 +24,21 @@ type HTTP struct {
 }
 
 type Auth struct {
-	URL string
+	URL          string
+	ResponseType string
+}
+
+type Client struct {
+	ID          string
+	Secret      string
+	RedirectURI string
 }
 
 // NewConfig returns config, incluedes environment vars.
 func NewConfig(cName, cPath, cType string) Config {
 	var c Config
 	common.InitConfig(cName, cPath, cType, func() {
+		viper.SetDefault("Auth.ResponseType", "code")
 		// var c Config
 		if err := viper.Unmarshal(&c); err != nil {
 			panic("config file unmarshal failed")
