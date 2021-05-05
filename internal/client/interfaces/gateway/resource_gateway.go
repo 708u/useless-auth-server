e584c6be012670a013c5a937920d24791b07e8df
708u <naoya.uda@showcase-gig.com>
2021-05-05 21:47:45 +0900

    feat: implement fetch resource temporal

--- internal/client/interfaces/gateway/resource_gateway.go
+++ internal/client/interfaces/gateway/resource_gateway.go
@@ -1,8 +1,46 @@
 package gateway
 
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+const resourceAPI = "/v1/user"
+
 type ResourceGateway struct{}
 
+type TmpResource struct {
+	Name        string `json:"name"`
+	Description string `json:"description"`
+}
+
 func (r *ResourceGateway) FetchUserResource(accessToken string) (string, error) {
-	// TODO: implement
-	return "", nil
+	// TODO: hard code change path from arg
+	req, _ := http.NewRequest(http.MethodPost, "http://localhost:9001"+resourceAPI, nil)
+	req.Header.Set("Authorization", "Bearer "+accessToken)
+	// api exec
+	client := http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return "", fmt.Errorf("failed ResourceGateway.FetchUserResource: %w", err)
+	}
+
+	if resp.StatusCode >= http.StatusMultipleChoices {
+		return "", fmt.Errorf("failed ResourceGateway.FetchUserResource: v1/user returned response code %s", resp.Status)
+	}
+
+	defer resp.Body.Close()
+	respBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	// TODO: temporal body
+	var tmp TmpResource
+	if err := json.Unmarshal(respBody, &tmp); err != nil {
+		return "", fmt.Errorf("failed ResourceGateway.FetchUserResource: %w", err)
+	}
+	return tmp.Name, nil
 }
