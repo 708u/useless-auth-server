55acd8f884efc160069540878913a62553e32ed0
708u <naoya.uda@showcase-gig.com>
2021-05-07 08:31:48 +0900

    feat: add fet user resource EP in auth

--- internal/auth/interfaces/controller/show_user_resource.go
+++ internal/auth/interfaces/controller/show_user_resource.go
@@ -1,17 +1,40 @@
 package controller
 
 import (
+	"fmt"
 	"net/http"
 
+	"github.com/708u/useless-auth-server/internal/auth/domain/usecase"
+	"github.com/708u/useless-auth-server/internal/pkg/interfaces/controller"
 	"github.com/708u/useless-auth-server/internal/pkg/interfaces/presenter"
-	"github.com/708u/useless-auth-server/internal/pkg/interfaces/presenter/json"
+	jsonRender "github.com/708u/useless-auth-server/internal/pkg/interfaces/presenter/json"
 )
 
 type ShowUserResource struct {
+	UseCase  usecase.GetUserResourceUseCase
 	Renderer presenter.Renderer
 }
 
+type ShowUserResourceResponse struct {
+	Name        string `json:"name"`
+	Description string `json:"description"`
+}
+
 func (s *ShowUserResource) Action(w http.ResponseWriter, r *http.Request) {
-	// TODO: temoral render
-	s.Renderer.Set(json.NewRenderHandler(w, r, nil, 200)).Render()
+	token, err := controller.ParseAccessTokenFromHeader(r)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("parsing token failed: %s", err.Error()), http.StatusUnauthorized)
+	}
+
+	out, err := s.UseCase.Handle(usecase.GetUserResourceInput{AccessToken: token})
+	if err != nil {
+		http.Error(w, "invalid access token", http.StatusUnauthorized)
+	}
+
+	resp := ShowUserResourceResponse{
+		Name:        out.Name,
+		Description: out.Description,
+	}
+	// TODO: temporal render
+	s.Renderer.Set(jsonRender.NewRenderHandler(w, r, resp, 200)).Render()
 }
